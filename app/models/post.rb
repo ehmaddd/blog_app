f367a50f4bea658b324dc63222d35aecb0a1b902
ehmaddd <ehmaddd@gmail.com>
2023-07-20 17:41:09 +0500

    Create Custom Methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.count)
+  end
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
