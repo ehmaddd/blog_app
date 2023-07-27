b3ea4ab2a9e586699420b6fbb6cae236542c6ea3
ehmaddd <ehmaddd@gmail.com>
2023-07-28 00:30:26 +0500

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :posts do
       resources :likes, only: [:create]
+      resources :comments, only: [:new, :create]
     end
   end
 end
