60a2e23fc929fc6bab1aa84d159a9a2259ec68ce
ehmaddd <ehmaddd@gmail.com>
2023-08-01 22:27:16 +0500

    Post Show Page Spec

---
+++ spec/factories/comment.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :comment do
+    text { "This is a comment." }
+    association :author, factory: :user # Assuming you have a valid association with the User model
+    association :post # Assuming you have a valid association with the Post model
+  end
+end
--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -3,5 +3,17 @@ FactoryBot.define do
     title { 'Sample Post Title' }
     text { 'This is the content of the post.' }
     association :author, factory: :user
+
+    trait :with_comments do
+      after(:create) do |post|
+        create_list(:comment, 2, post: post)
+      end
+    end
+
+    trait :with_likes do
+      after(:create) do |post|
+        create_list(:like, 5, post: post)
+      end
+    end
   end
 end
