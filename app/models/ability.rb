4b588d30204ee39e8dd1bd86b03bfcc095fd0742
ehmaddd <ehmaddd@gmail.com>
2023-08-04 14:14:11 +0500

    Apply suggested changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,12 @@ class Ability
   def initialize(user)
     user ||= User.new # Guest user (not logged in)
 
-    can :destroy, Post, author_id: user.id # Allow users to delete their own posts
-    can :destroy, Comment, author_id: user.id # Allow users to delete their own comments
-
-    return unless user.role == 'admin' # Check if the user has an admin role
-
-    can :destroy, Post # Allow admins to delete any post
-    can :destroy, Comment # Allow admins to delete any comment
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, Post, author: user  # owner of post can perform all view, update or destroy own post if Post.author == user
+      can :manage, Comment, author: user
+      can :read, :all
+    end
   end
 end
