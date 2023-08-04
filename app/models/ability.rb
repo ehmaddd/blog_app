514888d101bfbae06328c4629fc7091710e18429
ehmaddd <ehmaddd@gmail.com>
2023-08-04 14:22:23 +0500

    Resolve Linter Issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,7 @@ class Ability
     if user.admin?
       can :manage, :all
     else
-      can :manage, Post, author: user  # owner of post can perform all view, update or destroy own post if Post.author == user
+      can :manage, Post, author: user # owner of post can perform all view, update or destroy own post if Post.author == user
       can :manage, Comment, author: user
       can :read, :all
     end
