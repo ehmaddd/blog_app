4847257a216d54331300079216cb9c15090814ab
Patrick Mukula <Patrickkeita80@yahoo.com>
2023-08-04 21:35:44 -0700

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,28 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
 
+    if @comment.save
+      redirect_to @post, notice: 'Comment created successfully.'
+    else
+      render :new
+    end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    authorize! :destroy, @comment # Ensure user is authorized to delete the comment
 
+    if @comment.destroy
+      flash[:notice] = 'Comment was successfully deleted.'
+    else
+      flash[:alert] = 'Unable to delete the comment.'
+    end
+
+    redirect_to user_post_path(@post.author, @post)
   end
 
   private
