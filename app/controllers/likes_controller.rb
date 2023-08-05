a8d8e0b513f30838d7c907f93b69c71e384eff8d
Patrick Mukula <Patrickkeita80@yahoo.com>
2023-08-04 21:37:56 -0700

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,14 @@
 class LikesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.build(author: current_user)
 
+    if @like.save
+      flash[:notice] = 'You liked this post!'
+    else
+      flash[:alert] = 'Unable to like the post.'
+    end
+
+    redirect_to user_post_path(@post.author, @post)
   end
 end
